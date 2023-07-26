5448d90c997cf63bade5b17e080fb75f21caef86
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-26 20:04:03 +0300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstRange, secondRange) {
+    let finalSum = 0;
+    let maxRange;
+    let minRange;
+    if(typeof firstRange != 'number' ||typeof secondRange != 'number' || firstRange < 0 || secondRange < 0) {
+        return 'ERROR';
+    }
+    if(firstRange > secondRange) {
+        maxRange = firstRange;
+        minRange = secondRange;
+    } else {
+        maxRange = secondRange;
+        minRange = firstRange;
+    }
+    for(let i= maxRange; i >= minRange; i--) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
