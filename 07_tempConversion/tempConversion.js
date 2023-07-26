c4eb330063ed540b470979498f8ad88614b0d6be
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-26 22:10:02 +0300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(TempFahr) {
+  return Math.round(((TempFahr - 32) * (5/9)) * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(TempCelc) {
+  return Math.round(((TempCelc * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
