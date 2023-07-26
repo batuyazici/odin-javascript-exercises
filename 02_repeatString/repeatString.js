b222f7618abe77f1cd86d82ebff7fe4ed93a4cb7
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-26 16:45:21 +0300

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, count) {
+  let concatString = "";
+  if (count >= 0) {
+    for (let i = count; i > 0; i--) {
+      concatString += text;
+    }
+    return concatString;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
