979a818cc746eaab436d9cc895e87d23fc84f798
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-26 22:39:12 +0300

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 > num2) ? num1 - num2 : num2 - num1;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  return numberArray.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  return numberArray.reduce((acc,cur) => acc * cur);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(facNumber) {
+  let facResult = 1;
+	 while(facNumber > 0) {
+    facResult *= facNumber;
+    facNumber--;
+   }
+   return facResult;
 };
 
 // Do not edit below this line
