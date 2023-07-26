29ba9ba47bb1349048fae355b53ed5c592841f2c
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-26 17:27:13 +0300

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let chars = text.split('');
+    let reverseText = '';
+    
+    for(let i = chars.length - 1; i >= 0; i--)  {
+        reverseText += chars[i];
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
