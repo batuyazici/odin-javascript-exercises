f4885c0179511baacf8f7db8f93870f3b04eedcf
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-27 15:00:32 +0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/\W/g, '').toLowerCase();
+    let revText = text.split('').reverse().join('');
+    return text === revText;
+    
 };
 
 // Do not edit below this line
