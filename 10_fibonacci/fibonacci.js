819e37fd86384f2fcc46c01b28597c92fc851bcc
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-27 15:55:02 +0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(+number > 0) {
+    let previousNum = 0;
+    let fibonacciSum = 1;
+    let temp;
+    for(let i = +number - 1; i > 0; i--) {
+        temp = fibonacciSum;
+        fibonacciSum += previousNum;
+        previousNum = temp;
+    }
+    return fibonacciSum;
+} else {
+    return "OOPS";
+}
 };
 
 // Do not edit below this line
