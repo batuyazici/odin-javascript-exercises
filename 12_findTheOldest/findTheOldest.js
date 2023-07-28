eca9d28e48067d8629cec8159f3a777c21979018
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-28 14:21:40 +0300

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    return currentAge > oldestAge ? currentPerson : oldest;
+  });
 };
 
+function getAge(yearOfBirth, yearOfDeath = null) {
+  if (yearOfDeath !== null) {
+    calculatedAge = yearOfDeath - yearOfBirth;
+  } else {
+    calculatedAge = new Date().getFullYear() - yearOfBirth;
+  }
+  return calculatedAge;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
